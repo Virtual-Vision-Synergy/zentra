addd86ae1ada12d94446a0f7a46d936f99f13915
ManaJudy <judicaeljeanfrancois@gmail.com>
2025-10-23 16:40:33 +0200

    Initial Commit

---
+++ src/test/java/com/itu/business/zentra/ZentraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itu.business.zentra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZentraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
